da6817504961011598e341393a5b0ea9c6924e21
tx1990 <tx1990@yeah.net>
2018-01-26 23:09:13 +0800

    Container With Most Water

---
+++ LeetCode/ContainerWithMostWater.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            var l = 0;
+            var r = height.Length - 1;
+            var max = -1;
+            while (l < r)
+            {
+                var result = Math.Min(height[l], height[r]) * (r - l);
+                if (result > max)
+                {
+                    max = result;
+                }
+
+                if (height[r] > height[l])
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+            return max;
+        }
+    }
+}
